15aac26820a4146a5f09463a523dc6b7e7de91f3
Joseph Moloughney <jmoloughney25@gmail.com>
2017-11-16 18:38:30 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split(//)
   arr.sort_by do |word|
-    word.split(//).collect {|l| esperanto.index(l)}
+    word.split(//).collect {|letter| esperanto.index(letter)}
   end
 end
